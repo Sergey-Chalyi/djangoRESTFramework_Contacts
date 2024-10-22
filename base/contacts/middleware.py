0178ec8aa8bf0dd9207e2e69f7a20985e1395a9c
Sergey_Chalyi <supersupergray@gmail.com>
2024-10-22 16:45:16 +0300

    ADDED middlware for cheking users ip
    + if user isn't from UA/PL access would be forbidden

---
+++ base/contacts/middleware.py
@@ -0,0 +1,40 @@
+import requests
+from django.http import JsonResponse
+
+from base import settings
+
+ALLOWED_COUNTRIES = ['UA', 'PL']
+GEOLOCATION_URL = 'https://ipapi.co/{ip}/json/'
+
+class RegionRestrictionMiddleware:
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def get_client_ip(self, request):
+        if settings.DEBUG:  # Если включен режим разработки
+            return '88.130.76.202'
+
+        x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+        if x_forwarded_for:
+            ip = x_forwarded_for.split(',')[0]
+        else:
+            ip = request.META.get("REMOTE_ADDR")
+        return ip
+
+    def __call__(self, request):
+        ip_address = self.get_client_ip(request)
+
+        try:
+            response = requests.get(GEOLOCATION_URL.format(ip=ip_address))
+            data = response.json()
+            print(data)
+            country = data.get('country')
+            print(country)
+        except Exception as e:
+            print(f"Error: {e}")  # добавил для отладки
+            return JsonResponse({'details': "Couldn't get the information about ip"}, status=500)
+
+        if country not in ALLOWED_COUNTRIES:
+            return JsonResponse({'details': "Access denied!"}, status=403)
+
+        return self.get_response(request)
